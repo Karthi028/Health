e8df82280caddefa82d2bc6ccaaa1d0b1739e8ed
karthijayasangar <karthijayasangar6564@gmail.com>
2026-01-06 15:05:21 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
       console.log('Login successful:', response.data);
       toast.success('Login successful!')
       setStatusMessage('Login successful! Welcome back.');
-      navigate('/Home')
+      navigate('/')
       setIsLoggedIn(true);
 
     } catch (error) {
